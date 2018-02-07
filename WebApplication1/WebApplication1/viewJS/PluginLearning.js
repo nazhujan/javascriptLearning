597b056f1e69990613f1699d3f29d73425c150c7
nazhujan <nazrain>
2018-02-07 13:36:01 +0800

    commit

--- WebApplication1/WebApplication1/viewJS/PluginLearning.js
+++ WebApplication1/WebApplication1/viewJS/PluginLearning.js
@@ -1,1 +1,25 @@
 ﻿
+(function($){
+    $.fn.helloWorld = function (options) {
+        "use strict";
+        //Plugin Settings
+        var settings = $.extend({
+            text: 'Hello, World!',
+            color: null,
+            fontStyle: null
+        }, options);
+        //-End
+
+
+
+        return this.each(function () {
+            $(this).text(settings.text);
+            if (settings.color) {
+                $(this).css('color', settings.color);
+            }
+                
+        });
+    }
+
+
+}(jQuery));
